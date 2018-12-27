b12c190fbcb72f17f3d5516c4fc5a35593c0624b
MoonChang Chae <mcchae@vivans.net>
2018-12-27 17:49:15 +0900

    check error

--- git_pull.sh
+++ git_pull.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 LC_ALL=C git reset --hard origin/master
-LC_ALL=C git pull | tee dirty.txt | grep "Already up to date"
+LC_ALL=C git pull | tee dirty.txt 
+
+grep "error" dirty.txt
+if [ $? -eq 0 ];then
+	cat dirty.txt
+	echo "Error on git pull"
+	exit 9
+fi
+
+grep "Already up to date" dirty.txt
 if [ $? -eq 0 ];then
 	echo "Noting changed!"
 	rm -f dirty.txt
-	exit 1
+	exit 0
 else
 	echo "Something changed!"
 	cat dirty.txt
