49387d9eb225e9cd2ac40423d0b0b5560ae7901a
MoonChang Chae <mcchae@vivans.net>
2018-12-27 16:28:24 +0900

    use dirty.txt for dirty check

--- git_pull.sh
+++ git_pull.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 LC_ALL=C git reset --hard origin/master
-LC_ALL=C git pull | grep "Already up to date"
+LC_ALL=C git pull | tee dirty.txt | grep "Already up to date"
 if [ $? -eq 0 ];then
 	echo "Noting changed!"
+	rm -f dirty.txt
 	exit 1
 else
 	echo "Something changed!"
+	cat dirty.txt
 	exit 0
 fi
