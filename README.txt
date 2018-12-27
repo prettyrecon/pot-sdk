f3ec4ac3ca38395a7f224986721141a517f7293f
jerry linux dev auto build <mcchae@argos-labs.com>
2018-12-27 16:17:28 +0900

    add for git reset in test machine

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
+20181227 1616 - git down & auto build test for linux
 20181227 1519 - auto build test
--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+LC_ALL=C git reset --hard origin/master
 LC_ALL=C git pull | grep "Already up to date"
 if [ $? -eq 0 ];then
 	echo "Noting changed!"
