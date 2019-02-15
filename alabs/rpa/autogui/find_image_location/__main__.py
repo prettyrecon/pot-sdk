1b27f779a22a5117039e61c02d0baba04a09d3c2
임덕규 <deokyu@vivans.net>
2019-02-15 16:10:24 +0900

    # RPA 기본 모듈 추가: Find Image Location * 주어진 PNG 파일의 위치를 찾아서 반환

---
+++ alabs/rpa/autogui/find_image_location/__main__.py
@@ -0,0 +1,26 @@
+"""
+====================================
+ :mod:`argoslabs.demo.helloworld`
+====================================
+.. moduleauthor:: Raven Lim <deokyu@argos-labs.com>
+.. note:: VIVANS License
+
+Description
+===========
+ARGOS LABS plugin module sample
+"""
+
+################################################################################
+import sys
+from alabs.common.util.vvargs import ArgsError, ArgsExit
+from alabs.rpa.autogui.find_image_location import main
+
+
+################################################################################
+if __name__ == '__main__':
+    try:
+        main()
+    except ArgsError as err:
+        sys.stderr.write('Error: %s\nPlease -h to print help\n' % str(err))
+    except ArgsExit as _:
+        pass
