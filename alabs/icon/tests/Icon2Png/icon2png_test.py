2342f97685aa9027e949208fcab49737de62f00f
Jerry Chae <mcchae@Jerrys-MacBook-Pro.local>
2019-07-11 14:46:40 -0700

    add for alabs.icon

--- alabs/icon/tests/Icon2Png/icon2png_test.py
+++ alabs/icon/tests/Icon2Png/icon2png_test.py
@@ -3,13 +3,12 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 import sys
-import shutil
+# import shutil
 from contextlib import contextmanager
 from io import StringIO
 from icon_font_to_png import command_line
 from unittest import TestCase, TestLoader, TextTestRunner
-#
-# import pytest
+from PIL import Image
 
 
 ################################################################################
@@ -45,14 +44,59 @@ class TU(TestCase):
             self.assertTrue(True)
 
     # ==========================================================================
-    def test_0200_icon(self):
+    def test_0110_icon(self):
         try:
             command_line.run(
-                '--css fontawesome.css --ttf fontawesome-webfont.ttf --size 32 stop play car'.split()
+                '--css fontawesome.css --ttf fontawesome-webfont.ttf --size 30 '
+                '--color #6a76f6 car'.split()
             )
             # stdout = out.getvalue().strip()
             # print(stdout)
             # self.assertTrue(not stdout)
+            self.assertTrue(True)
+        except Exception as e:
+            sys.stderr.write('%s\n' % str(e))
+            self.assertTrue(False)
+
+    # ==========================================================================
+    def test_0120_icon_combine(self):
+        try:
+            im_blank = Image.open('icon_blank.png')
+            sz_blank = im_blank.size
+            im_icon = Image.open('exported/car.png')
+            sz_icon = im_icon.size
+            offset = int((sz_blank[0] - sz_icon[0]) / 2)
+            if offset < 5:
+                raise RuntimeError('Internal Icon is too big to fit in the frame')
+            im_blank.paste(im_icon, (offset, offset), im_icon)
+            im_blank.save('icon_car.png')
+            self.assertTrue(True)
+        except Exception as e:
+            sys.stderr.write('%s\n' % str(e))
+            self.assertTrue(False)
+
+    # ==========================================================================
+    def test_0200_foloder_eye(self):
+        try:
+            command_line.run(
+                '--css fontawesome.css --ttf fontawesome-webfont.ttf --size 30 '
+                '--color #403f3d folder'.split()
+            )
+            command_line.run(
+                '--css fontawesome.css --ttf fontawesome-webfont.ttf --size 20 '
+                '--color #6a76f6 eye'.split()
+            )
+
+            im_blank = Image.open('icon_blank.png')
+            sz_blank = im_blank.size
+            im_icons = map(Image.open, ('exported/folder.png', 'exported/eye.png'))
+            for im_icon in im_icons:
+                sz_icon = im_icon.size
+                offset = int((sz_blank[0] - sz_icon[0]) / 2)
+                if offset < 5:
+                    raise RuntimeError('Internal Icon is too big to fit in the frame')
+                im_blank.paste(im_icon, (offset, offset), im_icon)
+            im_blank.save('icon_folder_eye.png')
             self.assertTrue(True)
         except Exception as e:
             sys.stderr.write('%s\n' % str(e))
