ebfb8f9043a4441115bc9e2e9932aeade472ff73
mcchae@SG-UD18-01 <mcchae@argos-labs.com>
2019-09-24 19:16:53 -0700

    from San Jose for IBM demo

---
+++ linux-run-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#python -m alabs.ppm.tests.pam_api_caller http://192.168.1.101 ~/argoswork/Demo\ Bots/Data\ Lookup\ Sept2019/B\ Web\ Scraper\ -\ Data\ Lookup\ Process_UbuntuLinux.bot &
+#python -m alabs.ppm.tests.pam_api_caller http://192.168.1.102 ~/argoswork/Demo\ Bots/Data\ Lookup\ Sept2019/B\ Web\ Scraper\ -\ Data\ Lookup\ Process_UbuntuLinux.bot &
+#python -m alabs.ppm.tests.pam_api_caller http://192.168.1.103 ~/argoswork/Demo\ Bots/Data\ Lookup\ Sept2019/B\ Web\ Scraper\ -\ Data\ Lookup\ Process_UbuntuLinux.bot &
+#python -m alabs.ppm.tests.pam_api_caller http://192.168.1.104 ~/argoswork/Demo\ Bots/Data\ Lookup\ Sept2019/B\ Web\ Scraper\ -\ Data\ Lookup\ Process_UbuntuLinux.bot &
+
+python -m alabs.ppm.tests.pam_api_caller http://192.168.1.101 ~/argoswork/Demo\ Bots/State\ Cap\ Lookup/B3\ Lookup\ for\ State\ Capital\ -\ Ubuntu.bot &
+python -m alabs.ppm.tests.pam_api_caller http://192.168.1.102 ~/argoswork/Demo\ Bots/State\ Cap\ Lookup/B3\ Lookup\ for\ State\ Capital\ -\ Ubuntu.bot &
+python -m alabs.ppm.tests.pam_api_caller http://192.168.1.103 ~/argoswork/Demo\ Bots/State\ Cap\ Lookup/B3\ Lookup\ for\ State\ Capital\ -\ Ubuntu.bot &
+python -m alabs.ppm.tests.pam_api_caller http://192.168.1.104 ~/argoswork/Demo\ Bots/State\ Cap\ Lookup/B3\ Lookup\ for\ State\ Capital\ -\ Ubuntu.bot &
